1588a38c9957458e056df5a4e205fba2c92e9014
Sergey Rodin <rodin.s@rambler.ru>
2018-12-08 09:46:49 +0200

    Add toolbar and entry. Add entry callback

--- Gui.hpp
+++ Gui.hpp
@@ -1,5 +1,6 @@
 // Gui.hpp
 #include <string>
+#include <iostream>
 #include <gtk/gtk.h>
 
 using namespace std;
@@ -9,6 +10,9 @@ class Gui {
     Gui() {
 		const string PROG_NAME("Simple youtube");
         GtkWidget *window;
+        GtkWidget *toolbar;
+        GtkWidget *entry;
+        GtkWidget *vbox;
         
         gtk_init(0, NULL);
         
@@ -20,9 +24,38 @@ class Gui {
 	    // TODO: Add application icon
 	    //icon = IconsFactory::getAppIcon();
 		//gtk_window_set_icon(GTK_WINDOW(window), icon);
-		
 		gtk_window_set_default_size(GTK_WINDOW(window), 700, 400);
+		
+		// Toolbar
+		toolbar = gtk_toolbar_new();
+		gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
+	    gtk_container_set_border_width(GTK_CONTAINER(toolbar), 2);
+	    
+	    entry = gtk_entry_new();
+	    gtk_widget_set_tooltip_text(entry, "Search youtube");
+		GtkToolItem *entryItem = gtk_tool_item_new();
+		gtk_container_add(GTK_CONTAINER(entryItem), entry);
+		g_signal_connect(entry,
+                         "activate", 
+                         G_CALLBACK(entryActivated), 
+                         NULL);
+	    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), entryItem, -1);
+	    
+	    gtk_widget_show_all(toolbar);
+		
+		vbox = gtk_vbox_new(FALSE, 1);
+		gtk_box_pack_start(GTK_BOX(vbox), toolbar, FALSE, FALSE, 1);
+		gtk_widget_show(vbox);
+		gtk_container_add(GTK_CONTAINER(window), vbox);
 		gtk_widget_show(window);
 		gtk_main();
+    }
+    private:
+    
+    static void entryActivated( GtkWidget *widget, void* data) {
+        string query(gtk_entry_get_text(GTK_ENTRY(widget)));
+        if(!query.empty()) {
+	        cout << query << endl;
+	    }		  						  
     }
 };
